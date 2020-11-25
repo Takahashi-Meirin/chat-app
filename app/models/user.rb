e081dbc377247762ad2d7cfdee2f393b236797c7
Ramrin <iwgmw.nir02@gmail.com>
2020-11-25 11:43:29 +0900

    メッセージテーブル

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatabl,
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
 
   validates :name, presence: true
 end
